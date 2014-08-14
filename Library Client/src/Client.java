218fc74b6e40e0723c0b5c943ef496c185a2210f
Matt Gilene <mdgilene@yahoo.com>
2014-08-14 16:45:15 -0400

    Added client book list

--- Library Client/src/Client.java
+++ Library Client/src/Client.java
@@ -1,14 +1,20 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+
 
 public class Client {
 	
 	public static final String[] DATA_NAMES = {"Name", "Age"};
 	
-	String name;
-	int age;
+	private String name;
+	private int age;
+	private ArrayList<Book> books;
+	
 	
 	public Client(String name, int age) {
 		this.name = name;
 		this.age = age;
+		this.books = new ArrayList<Book>();
 	}
 	
 	public Client() {
@@ -16,6 +22,6 @@ public class Client {
 	}
 	
 	public Object[] asArray() {
-		return new Object[] {name, age};
+		return new Object[] {name, age, books.size()};
 	}
 }
